495f32bb7b66cfaa3cc3bd9ce26b5ead7896d58a
sonpipe <pipefc2003@gmail.com>
2024-05-24 20:31:39 -0300

    chat controller added missing dto's

---
+++ src/main/java/com/scholarsync/server/controllers/ChatController.java
@@ -0,0 +1,87 @@
+package com.scholarsync.server.controllers;
+
+import com.scholarsync.server.dtos.MessageFromUserDTO;
+import com.scholarsync.server.entities.Group;
+import com.scholarsync.server.entities.User;
+import com.scholarsync.server.repositories.GroupRepository;
+import com.scholarsync.server.repositories.UserRepository;
+import com.scholarsync.server.services.ChatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+
+@Controller
+public class ChatController {
+
+  @Autowired ChatService chatService;
+  @Autowired private UserRepository userRepository;
+  @Autowired
+  private GroupRepository groupRepository;
+
+  @MessageMapping("/chat/join")
+  public void joinChat(String userId, String chatId) {
+    chatService.joinChat(userId, chatId);
+  }
+
+  @MessageMapping("/chat/leave")
+  public void leaveChat(String userId, String chatId) {
+    chatService.leaveChat(userId, chatId);
+  }
+
+  @MessageMapping("/chat/send-message")
+  public void sendMessage(MessageFromUserDTO messageFromUserDTO) {
+    chatService.sendChatMessage(messageFromUserDTO);
+  }
+
+  @PostMapping("/api/v1/chat/upload-file")
+  public void uploadFile(
+      @RequestParam MultipartFile file, @RequestParam String userId, @RequestParam String chatId)
+      throws IOException {
+    chatService.uploadFile(file, chatId, userId);
+  }
+
+  @PostMapping("/api/v1/chat/create-chat")
+  public ResponseEntity<Object> createChat(@RequestBody Map<String, String> body) {
+    String userId = body.get("userId");
+    String groupId = body.get("groupId");
+    String name = body.get("name");
+    Optional<User> userOptional = userRepository.findById(userId);
+    if (userOptional.isEmpty()) {
+      return ResponseEntity.status(404).body("user/not-found");
+    }
+    User user = userOptional.get();
+    Optional<Group> optionalGroup = groupRepository.findById(groupId);
+    if (optionalGroup.isEmpty()) {
+      return ResponseEntity.status(404).body("group/not-found");
+    }
+    Group group = optionalGroup.get();
+    return chatService.createChat(group, name, user);
+  }
+
+  @PostMapping("/api/v1/chat/delete-chat")
+  public ResponseEntity<Object> deleteChat(@RequestBody Map<String, String> body) {
+    String userId = body.get("userId");
+    String chatId = body.get("chatId");
+    return chatService.deleteChat(chatId, userId);
+  }
+
+
+  @MessageMapping("/chat/delete-file")
+  public void deleteChat(String fileId, String chatId, String userId) {
+    chatService.deleteChatFile(fileId, chatId, userId);
+  }
+
+
+
+
+
+}
--- src/main/java/com/scholarsync/server/services/ChatService.java
+++ src/main/java/com/scholarsync/server/services/ChatService.java
@@ -35,18 +35,51 @@ public class ChatService {
     this.sender = sender;
   }
 
-  public void deleteChat(String chatId) {
+  public ResponseEntity<Object> deleteChat(String chatId, String userId) {
+    Optional<User> user = userRepository.findById(userId);
+    if (user.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("user/not-found");
+    Optional<Chat> chat = chatRepository.findById(chatId);
+    if (chat.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("chat/not-found");
+    if (!isUserInGroup(user.get(), chat.get().getGroup()))
+      return ResponseEntity.status(HttpStatus.FORBIDDEN).body("user/not-in-chat");
+    for(Files file: chat.get().getFiles()){
+      fileRepository.deleteById(file.getId());
+    }
+    for(User chatUser: chat.get().getGroup().getUsers()){
+      sender.convertAndSend("/individual/" + chatUser.getId() + "/chat", "chat/deleted");
+    }
     chatRepository.deleteById(chatId);
+    return ResponseEntity.ok("chat/deleted");
   }
 
-  public void deleteChatFile(String chatFileId) {
+  public void deleteChatFile(String chatFileId, String chatId, String userId) {
+    Optional<Files> chatFile = fileRepository.findById(chatFileId);
+    if (chatFile.isEmpty()) {
+      ErrorMessage errorMessage = new ErrorMessage();
+      errorMessage.setMessage("File not found");
+      errorMessage.setError("file/not-found");
+      sender.convertAndSend("/individual/" + chatFileId + "/error", errorMessage);
+      return;
+    }
+    Optional<User> userOptional = userRepository.findById(userId);
+    if (userOptional.isEmpty()) {
+      userNotFoundError(userId);
+      return;
+    }
+    User user = userOptional.get();
+    Optional<Chat> chat = chatRepository.findById(chatId);
+    if (chat.isEmpty()) {
+      chatNotFoundError(userId);
+    }
+    Files file = chatFile.get();
+    sender.convertAndSend("/chat/" + chatId, "file " + file.getFileName() + " deleted");
     fileRepository.deleteById(chatFileId);
   }
 
-  public void createChat(Group group, String name, User user) {
+  public ResponseEntity<Object> createChat(Group group, String name, User user) {
 
-    if (!isUserInGroup(user, group)) return;
-    if (checkCompositeKey(name, group.getId())) return;
+    if (!isUserInGroup(user, group)) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("user/not-in-group");
+    if (checkCompositeKey(name, group.getId())) return ResponseEntity.status(HttpStatus.CONFLICT).body("chat/exists");
 
     Chat chat = new Chat();
     chat.setGroup(group);
@@ -54,6 +87,7 @@ public class ChatService {
     chatRepository.save(chat);
 
     notifyGroupMembers(group, chat);
+    return ResponseEntity.ok("chat/created");
   }
 
   private void notifyGroupMembers(Group group, Chat chat) {
@@ -65,23 +99,47 @@ public class ChatService {
     }
   }
 
-  public void joinChat(User user, String chatId) {
+  public void joinChat(String userId, String chatId) {
+    Optional<User> userOptional = userRepository.findById(userId);
+    if (userOptional.isEmpty()) {
+      userNotFoundError(userId);
+      return;
+    }
+    User user = userOptional.get();
+    boolean userIsLoggedIn = sessionRepository.existsByUserId(userId);
+    if (!userIsLoggedIn) {
+      userNotLoggedInError(userId);
+      return;
+    }
     Optional<Chat> chat = chatRepository.findById(chatId);
-    if (chat.isEmpty()) return;
+    if (chat.isEmpty()) {
+      chatNotFoundError(userId);
+      return;
+    }
     chat.get().setUserCount(chat.get().getUserCount() + 1);
     chatRepository.save(chat.get());
     // send notification user joined chat
     sender.convertAndSend("/chat/" + chatId, "user " + user.getUsername() + " joined chat");
   }
 
-  public void leaveChat(User user, String chatId) {
+
+  public void leaveChat(String userId, String chatId) {
+    Optional<User> userOptional = userRepository.findById(userId);
+    if (userOptional.isEmpty()) {
+      userNotFoundError(userId);
+      return;
+    }
+    User user = userOptional.get();
     Optional<Chat> chat = chatRepository.findById(chatId);
-    if (chat.isEmpty()) return;
+    if (chat.isEmpty()) {
+      chatNotFoundError(userId);
+      return;
+    }
     chat.get().setUserCount(chat.get().getUserCount() - 1);
     chatRepository.save(chat.get());
-    // send notification user left chat
     sender.convertAndSend("/chat/" + chatId, "user " + user.getUsername() + " left chat");
   }
+
 
   public List<Chat> getActiveChatsByGroup(Group group) {
     return chatRepository.findByGroup(group);
@@ -90,35 +148,30 @@ public class ChatService {
   public void sendChatMessage(MessageFromUserDTO payload) {
     Optional<User> user = userRepository.findById(payload.getSenderId());
     if (user.isEmpty()) {
-      ErrorMessage errorMessage = new ErrorMessage();
-      errorMessage.setMessage("User not found");
-      errorMessage.setError("user/not-found");
-      sender.convertAndSend("/individual/" + payload.getSenderId() + "/error", errorMessage);
+      userNotFoundError(payload.getSenderId());
       return;
     }
     Optional<Chat> chat = chatRepository.findById(payload.getChatId());
     if (chat.isEmpty()) {
-      ErrorMessage errorMessage = new ErrorMessage();
-      errorMessage.setMessage("Chat not found");
-      errorMessage.setError("chat/not-found");
-      sender.convertAndSend("/individual/" + payload.getSenderId() + "/error", errorMessage);
+      chatNotFoundError(payload.getSenderId());
       return;
     }
     if (!isUserInGroup(user.get(), chat.get().getGroup())) {
-      ErrorMessage errorMessage = new ErrorMessage();
-      errorMessage.setMessage("User not in chat");
-      errorMessage.setError("user/not-in-chat");
-      sender.convertAndSend("/individual/" + payload.getSenderId() + "/error", errorMessage);
+      userNotInGroupError(payload);
       return;
     }
     MessageFromServerDTO message = MessageFromServerDTO.fromUserToServer(payload, user.get());
     sender.convertAndSend("/chat/" + payload.getChatId(), message);
   }
 
-  public boolean isUserInGroup(User user, Group group) {
-    Set<User> users = group.getUsers();
-    return group.getUsers().contains(user);
+  private void userNotInGroupError(MessageFromUserDTO payload) {
+    ErrorMessage errorMessage = new ErrorMessage();
+    errorMessage.setMessage("User not in chat");
+    errorMessage.setError("user/not-in-chat");
+    sender.convertAndSend("/individual/" + payload.getSenderId() + "/error", errorMessage);
+    return;
   }
+
 
   @Transactional
   public ResponseEntity<Object> uploadFile(MultipartFile file, String chatId, String userId)
@@ -144,8 +197,40 @@ public class ChatService {
     return ResponseEntity.ok("file/uploaded");
   }
 
-  public boolean checkCompositeKey(String name, String groupId) {
+  private boolean checkCompositeKey(String name, String groupId) {
     return chatRepository.existsByNameAndGroupId(name, groupId);
+  }
+
+  private boolean isUserInGroup(User user, Group group) {
+    Set<User> users = group.getUsers();
+    return group.getUsers().contains(user);
+  }
+
+
+  private void chatNotFoundError(String userId) {
+    ErrorMessage errorMessage = new ErrorMessage();
+    errorMessage.setMessage("Chat not found");
+    errorMessage.setError("chat/not-found");
+    sender.convertAndSend("/individual/" + userId + "/error", errorMessage);
+    return;
+  }
+
+  private void userNotLoggedInError(String userId) {
+    ErrorMessage errorMessage = new ErrorMessage();
+    errorMessage.setMessage("User not logged in");
+    errorMessage.setError("user/not-logged-in");
+    sender.convertAndSend("/individual/" + userId + "/error", errorMessage);
+    return;
+  }
+
+
+
+  private void userNotFoundError(String userId) {
+    ErrorMessage errorMessage = new ErrorMessage();
+    errorMessage.setMessage("User not found");
+    errorMessage.setError("user/not-found");
+    sender.convertAndSend("/individual/" + userId + "/error", errorMessage);
+    return;
   }
 
 }
