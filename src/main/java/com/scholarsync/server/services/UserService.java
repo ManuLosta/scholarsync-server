fa9b3f35230f4fa3a858965fc024a2274ee1afdf
sonpipe <pipefc2003@gmail.com>
2024-05-28 09:59:04 -0300

    fixed image service [bug]

--- src/main/java/com/scholarsync/server/services/UserService.java
+++ src/main/java/com/scholarsync/server/services/UserService.java
@@ -1,8 +1,10 @@
 package com.scholarsync.server.services;
 
+import com.scholarsync.server.dtos.FileDTO;
 import com.scholarsync.server.dtos.ProfileDTO;
 import com.scholarsync.server.entities.Files;
 import com.scholarsync.server.entities.User;
+import com.scholarsync.server.repositories.FileRepository;
 import com.scholarsync.server.repositories.UserRepository;
 
 import java.io.IOException;
@@ -18,6 +20,8 @@ import org.springframework.web.multipart.MultipartFile;
 public class UserService {
 
   @Autowired private UserRepository userRepository;
+  @Autowired
+  private FileRepository fileRepository;
 
   public ResponseEntity<Object> getIdByUsername(Map<String, String> username) {
     Optional<User> user = userRepository.findUserByUsername(username.get("username"));
@@ -44,6 +48,12 @@ public class UserService {
     if (picture == null) {
       return ResponseEntity.badRequest().body("file/not-found");
     }
+    Files previousFile = updatedUser.getProfilePicture();
+    if (previousFile != null) {
+      String id = previousFile.getId();
+      updatedUser.setProfilePicture(null);
+      fileRepository.deleteById(id);
+    }
     Files file = new Files();
     file.setFileName(picture.getOriginalFilename());
     file.setFileType(picture.getContentType());
@@ -63,8 +73,10 @@ public class UserService {
     if (foundUser.getProfilePicture() == null) {
       return ResponseEntity.badRequest().body("profile-picture/not-found");
     }
-    byte[] profilePicture = foundUser.getProfilePicture().getFile();
-    String encodedString = Base64.getEncoder().encodeToString(profilePicture);
-    return ResponseEntity.ok(encodedString);
+    FileDTO fileDTO = FileDTO.fileToDTO(foundUser.getProfilePicture());
+    HashMap<String, Object> response = new HashMap<>();
+    response.put("file", fileDTO);
+    response.put("base64Encoding", Base64.getEncoder().encodeToString(foundUser.getProfilePicture().getFile()));
+    return ResponseEntity.ok(response);
   }
 }
